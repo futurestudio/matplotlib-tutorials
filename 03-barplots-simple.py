6c24309625f56e89f36ac0ba1f6b7b2d7bfa3434
Norman Peitek <norman@peitek.com>
2019-07-21 10:58:31 +0200

    finalize bar plots

--- 03-barplots-simple.py
+++ 03-barplots-simple.py
@@ -34,12 +34,27 @@ def barplot_color2():
     plt.show()
 
 
-def barplot_vertical():
+def barplot_color3():
     year = [2014, 2015, 2016, 2017, 2018, 2019]
     tutorial_count = [39, 117, 111, 110, 67, 29]
-    plt.barh(year, tutorial_count, color="#6c3376", edgecolor="#409240", linewidth=2)
 
-    plt.xlabel('Number of futurestud.io Tutorials')
-    plt.ylabel('Year')
+    plt.bar(year[:-1], tutorial_count[:-1], color="#6c3376")
+    plt.bar(year[-1], tutorial_count[-1], color="#6c3376", edgecolor="#409240", linewidth=2)
+
+    plt.xlabel('Year')
+    plt.ylabel('Number of futurestud.io Tutorials')
+
+    plt.show()
+
+
+def barplot_horizontal():
+    year = [2014, 2015, 2016, 2017, 2018, 2019]
+    tutorial_count = [39, 117, 111, 110, 67, 29]
+
+    plt.barh(year[:-1], tutorial_count[:-1], color="#6c3376")
+    plt.barh(year[-1], tutorial_count[-1], color="#6c3376", edgecolor="#409240", linewidth=2)
+
+    plt.xlabel('Year')
+    plt.ylabel('Number of futurestud.io Tutorials')
 
     plt.show()
